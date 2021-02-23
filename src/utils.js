457cae73aa54ce4441b67826f68d2351280e4c52
Nomi <itsnomihere@gmail.com>
2021-02-23 18:41:33 +1100

    completed all the necassary util method for zsync

--- src/utils.js
+++ src/utils.js
@@ -53,5 +53,64 @@ async function depositToZkSync(zkSyncWallet, token, amountToDeposit, ethers) {
 }
 
 async function transfer(from, toAddress, amountToTransfer, transferFee, token, zksync, ethers) {
-    // Start here
+    const closestPackableAmount = zksync.utils.closestPackableTransactionAmount(
+        ethers.utils.parseEther(amountToTransfer))
+    const closestPackableFee = zksync.utils.closestPackableTransactionFee(
+        ethers.utils.parseEther(transferFee))
+
+    const transfer = await from.syncTransfer({
+        to: toAddress,
+        token: token,
+        amount: closestPackableAmount,
+        fee: closestPackableFee
+    })
+    const transferReceipt = await transfer.awaitReceipt()
+    console.log('Got transfer receipt.')
+    console.log(transferReceipt)
+}
+
+async function getFee(transactionType, address, token, zkSyncProvider, ethers) {
+    const feeInWei = await zkSyncProvider.getTransactionFee(transactionType, address, token)
+    return ethers.utils.formatEther(feeInWei.totalFee.toString())
+}
+
+async function withdrawToEthereum(wallet, amountToWithdraw, withdrawalFee, token, zksync, ethers) {
+    const closestPackableAmount = zksync.utils.closestPackableTransactionAmount(ethers.utils.parseEther(amountToWithdraw))
+    const closestPackableFee = zksync.utils.closestPackableTransactionFee(ethers.utils.parseEther(withdrawalFee))
+    const withdraw = await wallet.withdrawFromSyncToEthereum({
+        ethAddress: wallet.address(),
+        token: token,
+        amount: closestPackableAmount,
+        fee: closestPackableFee
+    })
+    await withdraw.awaitVerifyReceipt()
+    console.log('ZKP verification is complete')
+}
+
+async function displayZkSyncBalance(wallet, ethers) {
+    const state = await wallet.getAccountState()
+
+    if (state.committed.balances.ETH) {
+        console.log(`Commited ETH balance for ${wallet.address()}: ${ethers.utils.formatEther(state.committed.balances.ETH)}`)
+    } else {
+        console.log(`Commited ETH balance for ${wallet.address()}: 0`)
+    }
+
+    if (state.verified.balances.ETH) {
+        console.log(`Verified ETH balance for ${wallet.address()}: ${ethers.utils.formatEther(state.verified.balances.ETH)}`)
+    } else {
+        console.log(`Verified ETH balance for ${wallet.address()}: 0`)
+    }
+}
+
+module.exports = {
+    getZkSyncProvider,
+    getEthereumProvider,
+    depositToZkSync,
+    registerAccount,
+    displayZkSyncBalance,
+    transfer,
+    withdrawToEthereum,
+    getFee,
+    initAccount
 }
